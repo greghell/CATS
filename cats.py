a1956e4d1f5c4dc9ef985d899761bb3ded18525d
greghell <greg.hellbourg@gmail.com>
2018-08-30 17:19:07 -0700

    Update cats.py

--- cats.py
+++ cats.py
@@ -15,8 +15,6 @@ timestr = time.strftime("%Y%m%d-%H%M%S")
 dirname = 'CATS_'+timestr
 os.mkdir(dirname)
 os.chdir(dirname)
-import sys
-sys.stdout = open('FullLog.txt', 'w')
 print 'creating log directory ' + os.getcwd()
 
 # list dibas directories
@@ -79,5 +77,42 @@ print str(DiagRAW) + ' DIAG RAW files found on BLC' + CompNodeNum[0] + ' - liste
 print str(BLCnonDiag) + ' non DIAG RAW files found on BLC' + CompNodeNum[0] + ' - listed in ' + BLCnonDiagLog + ' => TO GPUSPEC'
 print str(GUPPInonDiag) + ' non DIAG GUPPI RAW files found on BLC' + CompNodeNum[0] + ' - listed in ' + GUPPInonDiagLog + ' => TO RE-GPUSPEC'
 print str(NonStdNonDiag) + ' non standard RAW files found on BLC' + CompNodeNum[0] + ' - listed in ' + NonStdnonDiagLog
+
+# find FIL files associated with guppi raw files
+GUPPINonDiagRawlist = open(GUPPInonDiagLog, 'r')
+guppilist = GUPPINonDiagRawlist.readlines()
+GuppiFILnum = 0
+Guppi_NO_FILnum = 0
+guppiFILlog = 'guppi_fil_filesBLC' + CompNodeNum[0] + '.txt'
+guppi_without_FILlog = 'guppi_without_fil_filesBLC' + CompNodeNum[0] + '.txt'
+guppiFILlist = open(guppiFILlog, 'w')
+guppi_no_FILlist = open(guppi_without_FILlog, 'w')
+old_fname = ''
+for rawfile in guppilist:
+    fname =  rawfile.split("/")[-1]
+    if len(fname) > 11:
+        corename = fname[:-10]
+        pathtofile = os.path.dirname(rawfile)
+        if old_fname != corename:
+            filfiles = glob(pathtofile + '/*' + corename + '*.fil')
+            if len(filfiles) > 0:
+                GuppiFILnum = GuppiFILnum+1
+                for filfilename in filfiles:
+                    guppiFILlist.write(filfilename + '\n')
+            else:
+                Guppi_NO_FILnum = Guppi_NO_FILnum + 1
+                guppi_no_FILlist.write(rawfile)
+    old_fname = corename
+print 'found ' + str(GuppiFILnum) + ' FIL files datasets associated with GUPPI RAW files on BLC' + CompNodeNum[0] + ' - listed in ' + guppiFILlog + ' -> to delete and try to re-GPUSPEC'
+print 'found ' + str(Guppi_NO_FILnum) + ' GUPPI RAW files datasets without FIL files on BLC' + CompNodeNum[0] + ' - listed in ' + guppi_without_FILlog + ' -> try to re-GPUSPEC'
+guppiFILlist.close()
+guppi_no_FILlist.close()
+
+
+
+
+
+
+
 
 
